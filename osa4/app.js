18d30cdc13a3bbb46961f20d15ec39ec265994b8
Milla Lintunen <milla.lintunen@hotmail.com>
2019-10-22 12:24:27 +0300

    4.1-4.2

---
+++ osa4/app.js
@@ -0,0 +1,28 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+
+
+  
+  
+  mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  
+  app.use(cors())
+  app.use(express.static('build'))
+  app.use(bodyParser.json())
+  app.use(middleware.requestLogger)
+
+  app.use('/api/blogs', blogsRouter)
+
+  app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+  module.exports = app
+  
+ 
