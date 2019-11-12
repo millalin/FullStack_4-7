4a40360181d8b9fa15230efd727de361e89473b8
Milla Lintunen <milla.lintunen@hotmail.com>
2019-11-12 15:58:10 +0200

    myös 5.15 ok

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,68 +1,66 @@
-import React, { useState } from 'react'
-import blogService from '../services/blogs'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import blogService from '../services/blogs'
+import PropTypes from 'prop-types'
 
-const Blog = ({ blog, un }) => {
+const Blog = ({ blog, un }) => {
 
-Blog.propTypes  = {
-  blog: PropTypes.object.isRequired,
-  un: PropTypes.string.isRequired
+Blog.propTypes  = {
+  blog: PropTypes.object.isRequired,
+  un: PropTypes.string.isRequired
 }
-  const [showinfo, setShowInfo] = useState(true)
+  const [showinfo, setShowInfo] = useState(true)
 
+  const deleteBlog = () => {
+    if (window.confirm(`Remove ${blog.title} by ${blog.author} ?`)){
+      blogService.remove(blog.id)
+    }
+  }
 
-  const deleteBlog = () => {
-    if (window.confirm(`Remove ${blog.title} by ${blog.author} ?`)){
-      blogService.remove(blog.id)
-    }
-  }
+  const delButton = () => {
+  
+   if (un !== blog.user.username){
+      return (
+        <div></div>
+      )
+    }
 
-  const delButton = () => {
-  
-   if (un !== blog.user.username){
-      return (
-        <div></div>
-      )
-    }
-
-    return (
+    return (
       <div>
-      <button onClick={deleteBlog}>remove</button>
-    </div>
+        <button onClick={deleteBlog}>remove</button>
+      </div>
     )
   }
 
   const likeAdding = () => {
     blog.likes = blog.likes + 1
-    
+
     blogService.update(blog)
-    
+
   }
 
   const showBlog = () => {
     setShowInfo(!showinfo)
   }
 
- 
 
   if (showinfo) {
     return (
-      <div onClick={showBlog}>
-        <p>Title: <b> {blog.title}  </b>Author: {blog.author}  </p>
-        <br></br>
+      <div  onClick={showBlog} >
+       <div> <p>Title: <b> {blog.title}  </b>Author: {blog.author}  </p>
+        <br></br></div>
       </div>)
   } else {
     return (
-      <div className="showall" onClick={showBlog} >
-        <p>Title: <b> {blog.title}  </b>Author: {blog.author}  </p>
-        <p>Url: {blog.url} Likes: {blog.likes}</p>
+      <div onClick={showBlog} >
+        <div ><p>Title: <b> {blog.title}  </b>Author: {blog.author}  </p>
+        <p>Url: {blog.url} Likes: {blog.likes}</p></div>
         <button onClick={likeAdding}>like</button>
         {delButton()}
-        <br></br>
+        <br></br> 
       </div>
 
     )
   }
 }
 
-export default Blog
+export default Blog
--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,38 +1,36 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 
-
-
 describe('<Blog />', () => {
+
     let component
 
-    
-        const blog = {
-            title: 'Test title',
-            author: 'auth',
-            url: 'url',
-            likes: 2,
-            user: {
-                username: "Kalle",
-                name: "Kalle",
-                id: 12341234
-            },
-            id: 11223344
-        }
 
-        component = render(
-            <Blog blog={blog} un='Kalle'/>
-               
-            
-        )
-    
+    const blog = {
+        title: 'Test title',
+        author: 'auth',
+        url: 'url',
+        likes: 2,
+        user: {
+            username: "Kalle",
+            name: "Kalle",
+            id: 12341234
+        },
+        id: 11223344
+    }
+
+
 
 
 
     test('renders content', () => {
+        component = render(
+            <Blog blog={blog} un='Kalle' />
 
+
+        )
 
         expect(component.container).toHaveTextContent(
             'Test title'
@@ -53,16 +51,22 @@ describe('<Blog />', () => {
     })
 
     test('renders content when pressed', () => {
+        component = render(
+            <Blog blog={blog} un='Kalle' />
 
 
-        const show = component.container.querySelector('.showall')
-        
-        expect(show).toHaveTextContent('Test title')
-        expect(show).toHaveTextContent('url') 
+        )
+        const press = component.getByText('Test title')
+        fireEvent.click(press)
 
+        expect(component.container).toHaveTextContent('Test title')
+        expect(component.container).toHaveTextContent('url')
+        expect(component.container).toHaveTextContent(
+            '2'
+        )
     })
 
 
- 
+
 
 }) 
