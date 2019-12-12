70d3e642741aebf61e5e88f52b2e4f40883b25ca
Milla Lintunen <milla.lintunen@hotmail.com>
2019-12-12 13:16:43 +0200

    osa 7 backend

--- osa7/backend/bloglist-backend/app.js
+++ osa7/backend/bloglist-backend/app.js
@@ -1,16 +1,14 @@
-
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
 const { tokenExtractor, errorHandler } = require('./utils/middleware')
-
 const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -27,11 +25,20 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
 app.use(tokenExtractor)
 
+
+
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
 app.use(errorHandler)
+
+
+if (process.env.NODE_ENV === 'test'){
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 
 
 module.exports = app
